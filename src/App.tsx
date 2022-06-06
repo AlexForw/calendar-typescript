aa6849658e21785a1f00c7f30545880d0d3a1cc9
AlexForw <lmityushkin@mail.ru>
2022-06-06 23:25:26 +0300

    feat: add createDate function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
-import BoardComponent from './components/BoardComponent';
 
 
 function App() {
   return (
     <div className="app">
-      <BoardComponent />
+      calendar
     </div>
   );
 }
