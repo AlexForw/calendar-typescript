830d83ccd5a96f7e9cd4a64d23b2ff89a39ffe37
AlexForw <lmityushkin@mail.ru>
2022-06-06 23:50:13 +0300

    feat: add createMonth function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
+import { createDate } from './date/createDate';
+import { createMonth } from './date/createMonth'
 
 
 function App() {
+  console.log(createDate({locale:'en-US'}));
+  
   return (
     <div className="app">
       calendar
