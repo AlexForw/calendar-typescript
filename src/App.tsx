f6ec0ce25c0c080c5801f005ca989533e65eaf9b
AlexForw <lmityushkin@mail.ru>
2022-06-08 23:44:57 +0300

    feat: add select language

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Calendar from './components/Calendar/Calendar';
 import { formatDate } from './date/formatDate';
@@ -7,11 +7,20 @@ import { formatDate } from './date/formatDate';
 
 function App() {
   const [selectedDate, selectDate] = useState(new Date())
+  const [localeState, setLocaleState] = useState('ru')
+  console.log(localeState);
 
   return (
     <div className="app">
+
+      <select name='language' onChange={(e) => setLocaleState(e.target.value)} className='change-language'>
+        <option value="ru">RU</option>
+        <option value="zh">CH</option>
+        <option value="en-US">EN</option>
+      </select>
+
       <div className="date__container">{formatDate(selectedDate, 'DD MM YYYY')}</div>
-      <Calendar selectDate={selectDate} selectedDate={selectedDate} />
+      <Calendar locale={localeState} selectDate={selectDate} selectedDate={selectedDate} />
     </div>
   );
 }
