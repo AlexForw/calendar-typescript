635c18207cc5ace5ab92724f363ed14413dbcb32
AlexForw <lmityushkin@mail.ru>
2022-06-08 23:18:55 +0300

    refactor: formating 2 functions

--- src/date/getMonthDays.ts
+++ src/date/getMonthDays.ts
@@ -1,4 +1,4 @@
 export const getMonthDays = (
-    monthIndex: number,
-    yearNumber: number = new Date().getFullYear()
-  ) => new Date(yearNumber, monthIndex + 1, 0).getDate();
+  monthIndex: number,
+  yearNumber: number = new Date().getFullYear()
+) => new Date(yearNumber, monthIndex + 1, 0).getDate();
